4afa3c42c1181fe199b23689ce89d38e1a5b8103
Rimkus Lukas <lukas.rimkus@procemex.com>
2019-12-06 16:41:37 +0200

    Updates

---
+++ precache-manifest.054dbfdb35b4ea9c20a566e8c4a93c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fullstackopenexamples/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "eb42d8a30c2dfa2a78ac",
+    "url": "/fullstackopenexamples/static/js/runtime~main.242edd46.js"
+  },
+  {
+    "revision": "5b68a6b6f1fb6577cba4",
+    "url": "/fullstackopenexamples/static/js/main.4773c1d7.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/fullstackopenexamples/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5b68a6b6f1fb6577cba4",
+    "url": "/fullstackopenexamples/static/css/main.bf19c27c.chunk.css"
+  },
+  {
+    "revision": "4a29bf98103cff7e60a7a0f5c1d452a3",
+    "url": "/fullstackopenexamples/index.html"
+  }
+];
