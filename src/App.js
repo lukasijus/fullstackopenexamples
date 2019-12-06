076e0fd5b222d812f4c5fbbdc35a73cc88c60103
Rimkus Lukas <lukas.rimkus@procemex.com>
2019-12-06 17:10:49 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Full Stack Open 2019 course examples react app on gh-pages.
-          </p>
-          <a
-            className="App-link"
-            href="https://lukasijus.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Checkout Lukasijus
-          </a>
-        </header>
       </div>
     );
   }
