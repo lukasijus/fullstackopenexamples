f48b52586b9b6442731d13d91f57ab4dc523deeb
Rimkus Lukas <lukas.rimkus@procemex.com>
2019-12-06 16:46:31 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Lukasijus react app on gh-pages.
+            Full Stack Open 2019 course examples react app on gh-pages.
           </p>
           <a
             className="App-link"
